ae418f97b6999c0c23219c3ed6c19a07be90b082
Freet8707 <freet8707@gmail.com>
2020-07-08 21:59:15 -0500

    initial array assigned to variable for to do list and state set up in app.js, todolist successfully renders tasks array values

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import ToDoList from './components/TodoList'
+
+const tasks = [
+  {
+    name: 'heyyo',
+    id: Date.now(),
+    completed: false
+  }
+]
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      tasks: tasks
+    }
+  }
+
   render() {
     return (
-      <div>
+      <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
         <h2>Welcome to your Todo App!</h2>
+        <ToDoList tasks={this.state.tasks} />
       </div>
     );
   }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,7 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react'
+
+const ToDoList = (props) => <div>{props.tasks.map(item => <h2 key={item.id}>{item.name}</h2>)}</div>
+
+export default ToDoList 
